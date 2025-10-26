a9e7c3e96c4199e9f0d754b2b1ea168749b85392
lMuffinn <mattheweagleman@gmail.com>
2025-10-26 17:20:09 -0500

    Update import_courses.py
    made better comments

--- courses/management/commands/import_courses.py
+++ courses/management/commands/import_courses.py
@@ -8,14 +8,14 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 from courses.models import Course
 
-class Command(BaseCommand):
+class Command(BaseCommand): #Makes it so that you can call it as a command using manage.py
     help = 'Imports course data from an Excel file into the database.'
 
     def add_arguments(self, parser):
         parser.add_argument('excel_file', type=str, help='The path to the Excel file')
 
     def handle(self, *args, **kwargs):
-        file_path = kwargs['excel_file']
+        file_path = kwargs['excel_file'] #Gets the file path from the terminal
         
         try:
             # Read the Excel file into a pandas DataFrame
@@ -24,9 +24,8 @@ class Command(BaseCommand):
             # Use bulk_create for efficient mass insertion
             courses_to_create = []
             
-            for _, row in df.iterrows():
-                # Assuming your Excel has columns like 'CourseID', 'Title', etc.
-                course = Course(
+            for _, row in df.iterrows(): # Iterates through each row of the excel file
+                course = Course( # Creates a course object for each row, and populates it with the appropriat info from the columns
                     subject=row['Course'],
                     course_number=int(row['Number']),
                     registrar_course_number=int(row['Course nbr']),
@@ -51,12 +50,11 @@ class Command(BaseCommand):
                     location=row['Location'],
                     room=row['Room']
                 )
-                print(course)
+                print(course) # prints the course its adding. we can get rid of this, but i kinda like seeing everything being added
                 courses_to_create.append(course)
-            print('about to create courses')
-            Course.objects.bulk_create(courses_to_create)
+            Course.objects.bulk_create(courses_to_create) #Adds courses to the database in one, optimized query
             self.stdout.write(self.style.SUCCESS(f'Successfully imported {len(courses_to_create)} courses.'))
-
+        # Catching errors
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR(f'File not found at: {file_path}'))
         except KeyError as e:
